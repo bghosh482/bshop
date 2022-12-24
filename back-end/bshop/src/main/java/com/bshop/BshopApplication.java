7fc562380be6153e1e8c08a2fa3a1f822ec628fa
Bubai Ghosh <bghosh482@gmail.com>
2022-12-24 14:13:49 +0530

    added

---
+++ back-end/bshop/src/main/java/com/bshop/BshopApplication.java
@@ -0,0 +1,13 @@
+package com.bshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BshopApplication.class, args);
+	}
+
+}
---
+++ back-end/bshop/src/test/java/com/bshop/BshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
